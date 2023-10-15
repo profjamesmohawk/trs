8691c80d3848e996d4cc907edcc477b2231db85d
james <james.long5@mohawkcollege.ca>
2023-10-15 19:07:01 -0400

    added support for env vars in config.php

--- php/config.php
+++ php/config.php
@@ -3,6 +3,17 @@
 	# set the database connection parameters
 	#
 
+	# my_getenv() adds a default value to getenv()
+	#
+	function my_envget($EnvVarName, $DefaultValue)
+	{
+		$EnvVarValue=getenv($EnvVarName);
+		if ( ! $EnvVarValue ) {
+			return $DefaultValue;
+		}
+		return $EnvVarValue;
+	}
+
 	# set the config file name
 	$ConfigFileName="/var/local/xtrs/config.php";
 
@@ -13,10 +24,11 @@
  	}
 	else {
 		# config file not found, use the default values
+		# Environment variables take precedence
 		# 
-		$trs_db_uid="trs_user";
-		$trs_db_pw="trs_pass";
-		$trs_db_host="db";
-		$trs_db_db="trs";
+		$trs_db_uid=my_envget("TRS_DB_UID","trs_user");
+		$trs_db_pw=my_envget("TRS_DB_PW", "trs_pass");
+		$trs_db_host=my_envget("TRS_DB_HOST","db");
+		$trs_db_db=my_envget("TRS_DB_DB","trs");
 	}
 ?>
