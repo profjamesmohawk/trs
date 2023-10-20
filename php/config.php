923d2e38a2428f1ba04e6a67a31d627927210206
james <james.long5@mohawkcollege.ca>
2023-10-20 16:21:43 -0400

    refactored config.php for readability

--- php/config.php
+++ php/config.php
@@ -1,9 +1,44 @@
 <?php
+	########################################################################
 	#
-	# set the database connection parameters
+	# Set the database connection parameters
+	#	trs_db_uid	- mariadb user name 
+	#	trs_db_pw	- mariadb pw to use for trs_db_uid
+	#	trs_db_host	- hostname or IP address of DB server
+	#	trs_db_db	- mariadb database name
 	#
+	# To support multiple deployment strategies we can set the configuraion
+	# variables in three places.  They are evaluated in the following order
+	# with later assignments overwriting previous ones.
+	#	- this file
+	#	- $ConfigFileName
+	#	- environment variables:
+	#			(TRS_DB_UID, TRS_DB_PW, TRS_DB_HOST, TRS_DB_DB)
+	#
+	# James, October 2023
+	########################################################################
 
-	# my_getenv() adds a default value to getenv()
+	# set the defaults
+	$trs_db_uid="trs_user";
+	$trs_db_pw= "trs_pass";
+	$trs_db_host="db";
+	$trs_db_db="trs";
+
+	# set the config file name, to override assignments made above
+	# place outside document root to increase security
+	$ConfigFileName="/var/local/xtrs/config.php";
+
+
+	############### no configuration changes after this line ###############
+
+
+	# Get the config values from $ConfigFileName, 
+	#
+	if ( file_exists($ConfigFileName) ){
+		include	$ConfigFileName;
+ 	};
+
+	# function my_getenv() adds a default value to getenv()
 	#
 	function my_envget($EnvVarName, $DefaultValue)
 	{
@@ -14,21 +49,9 @@
 		return $EnvVarValue;
 	}
 
-	# set the config file name
-	$ConfigFileName="/var/local/xtrs/config.php";
-
-	# Get the config values from $ConfigFileName, if we can
-	#
-	if ( file_exists($ConfigFileName) ){
-		include	$ConfigFileName;
- 	}
-	else {
-		# config file not found, use the default values
-		# Environment variables take precedence
-		# 
-		$trs_db_uid=my_envget("TRS_DB_UID","trs_user");
-		$trs_db_pw=my_envget("TRS_DB_PW", "trs_pass");
-		$trs_db_host=my_envget("TRS_DB_HOST","db");
-		$trs_db_db=my_envget("TRS_DB_DB","trs");
-	}
+	# overwrite values with those from envvariables where present
+	$trs_db_uid=my_envget("TRS_DB_UID",$trs_db_uid);
+	$trs_db_pw=my_envget("TRS_DB_PW", $trs_db_pw);
+	$trs_db_host=my_envget("TRS_DB_HOST",$trs_db_host);
+	$trs_db_db=my_envget("TRS_DB_DB",$trs_db_db);
 ?>
